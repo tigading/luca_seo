4db32ca204ec65a8b94966d729f09128b8c6ad53
Dinh Hoang Tuan <tuandh7x@gmail.com>
2022-01-05 20:32:02 +0700

    deploy seo

--- src/server.js
+++ src/server.js
@@ -133,6 +133,6 @@ app.get('/m/:token', (req, res) => {
 		});
 });
 
-app.listen(8888, () => {
-	console.log('Server started at port 8888');
+app.listen(3000, () => {
+	console.log('Server started at port 3000');
 });
